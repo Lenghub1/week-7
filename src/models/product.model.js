2d378166f143c45b3e13e63eeda8a840381c681f
SereyvatanaUng <sereyvatanau@gmail.com>
2023-10-30 15:17:09 +0700

    created product schema

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,29 +1,45 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    unique: true,
-    min: 3,
-    trim: true,
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 3,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      minLength: 10,
+      trim: true,
+    },
+    unitPrice: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    unit: {
+      type: String,
+      enum: ["item", "kg"],
+      default: "item",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    categories: [String],
   },
-  description: {
-    type: String,
-    required: true,
-    min: 10,
-    trim: true,
-  },
-  unitPrice: {
-    type: Number,
-    required: true,
-  },
-  unit: {
-    type: String,
-    enum: ["item", "kg"],
-    default: "item",
-  },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 const Product = mongoose.model("Product", productSchema);
 export default Product;
