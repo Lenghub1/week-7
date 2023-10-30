bfac894cc53dddcefbe9db7f981263778f2da6e6
SreyNann <soeunsreynan1604@gmail.com>
2023-10-30 13:42:16 +0700

    Add new product services

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,9 +1,41 @@
 import Product from "../models/product.model.js";
+import AppError from "../utils/AppError.js";
 
 const productService = {
   async createProduct(productInput) {
     const newProduct = new Product(productInput);
     await newProduct.save();
+  },
+
+  async getProduct(productId) {
+    const product = Product.findById(productId);
+    if (!product) {
+      return next(AppError("There is no document found with this ID.", 404));
+    }
+    return product;
+  },
+
+  async updateProduct(productId, productInput) {
+    const product = Product.findByIdAndUpdate(productId, productInput, {
+      new: true,
+      runValidators: true,
+    });
+    if (!product) {
+      return next(AppError("There is no document found with this ID.", 404));
+    }
+    return product;
+  },
+
+  async deleteProduct(productId) {
+    const product = Product.findByIdAndRemove(productId);
+    if (!product) {
+      return next(AppError("There is no document found with this ID.", 404));
+    }
+    return product;
+  },
+
+  async getAllProducts() {
+    return Product.find();
   },
 };
 
