052eb63a3f8d6eed1bbeade56c23f1626037053c
026 <anbschool0026@026s-Mac-mini.local>
2023-10-30 20:26:18 +0700

    implemented user Schema

---
+++ src/models/seller.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const sellerSchema = mongoose.Schema(
+  {
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    storeAddress: {
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    storeName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Seller = mongoose.model("Seller", sellerSchema);
+export default Seller;
---
+++ src/models/user.model.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    profilePicture: String,
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    passwordConfirm: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "seller", "admin"],
+      default: "user",
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpires: Date,
+    passwordChangeAt: Date,
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
