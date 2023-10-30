90018d78701bcdbc4ca43318c5a4e2545336dca5
SreyNann <soeunsreynan1604@gmail.com>
2023-10-30 13:41:06 +0700

    Implement factory function for manage common http requests

--- src/controllers/factory.js
+++ src/controllers/factory.js
@@ -1,13 +1,59 @@
+import catchAsync from "../utils/catchAsync.js";
+import AppError from "../utils/AppError.js";
+
 const factory = {
   createOne(createDocService) {
-    return async (req, res, next) => {
-      try {
-        await createDocService(req.body);
-        return res.json("Create a product");
-      } catch (error) {
-        next(error);
+    return catchAsync(async (req, res, next) => {
+      await createDocService(req.body);
+      res.json({ message: "Create a product" });
+    });
+  },
+
+  getById(getOneDoc) {
+    return catchAsync(async (req, res, next) => {
+      const doc = await getOneDoc(req.params.id);
+      res.json({
+        message: "Data Retrived",
+        data: { doc },
+      });
+    });
+  },
+
+  updateOne(updateOneDoc) {
+    return catchAsync(async (req, res, next) => {
+      const updateDoc = await updateOneDoc(req.params.id, req.body);
+      if (!doc) {
+        return next(AppError("There is no document found with this ID.", 404));
       }
-    };
+      res.json({
+        message: "Data Updated",
+        data: { updateDoc },
+      });
+    });
+  },
+
+  deleteOne(deleteOneDoc) {
+    return catchAsync(async (req, res, next) => {
+      const doc = await deleteOneDoc(req.params.id);
+      if (doc === undefined) {
+        return next(AppError("There is no document found with this ID.", 404));
+      }
+      res.json({
+        message: "Document Deleted",
+        data: null,
+      });
+    });
+  },
+
+  getAll(getAllDocs) {
+    return catchAsync(async (req, res, next) => {
+      const docs = await getAllDocs();
+      res.json({
+        message: "Data Retrived",
+        results: docs.length,
+        data: { docs },
+      });
+    });
   },
 };
 
