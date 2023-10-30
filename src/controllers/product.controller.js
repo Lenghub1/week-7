49195aa4c88e38632086e519cbc1b08425c1d087
SreyNann <soeunsreynan1604@gmail.com>
2023-10-30 13:44:36 +0700

    Build new product's controller fuctions

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,7 +3,10 @@ import factory from "./factory.js";
 
 const productController = {
   createProduct: factory.createOne(service.createProduct),
-  // getAllProducts: factory.getAll(service.)
+  getProduct: factory.getById(service.getProduct),
+  getAllProducts: factory.getAll(service.getAllProducts),
+  updateProduct: factory.updateOne(service.updateProduct),
+  deleteProduct: factory.deleteOne(service.deleteProduct),
 };
 
 export default productController;
