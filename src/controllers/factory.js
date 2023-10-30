1b9bc30a343cc690a9f718045b77c0c5f86bff6b
SreyNann <soeunsreynan1604@gmail.com>
2023-10-30 20:38:26 +0700

    Add Comment for common functions usage

--- src/controllers/factory.js
+++ src/controllers/factory.js
@@ -1,17 +1,37 @@
 import catchAsync from "../utils/catchAsync.js";
 
 const factory = {
+  /**
+   *
+   * This function is designed to be used as middleware in an Express.js route handler.
+   * It takes a request (req), response (res), and next middleware (next) as parameters.
+   * It invokes the 'createDocService' with the request body to create a new document.
+   * Once the document is created, it sends a JSON response indicating success.
+   *
+   * @param {Function} createDocService - A service function responsible for document creation.
+   * @returns {Function} Middleware function for document creation.
+   */
   create(createDocService) {
     return catchAsync(async (req, res, next) => {
       const newProduct = await createDocService(req.body);
 
       res.json({
-        message: "Create a product",
+        message: "Document Created",
         data: { newProduct },
       });
     });
   },
 
+  /**
+   *
+   * This function is designed to be used as middleware in an Express.js route handler.
+   * It takes a request (req), response (res), and next middleware (next) as parameters.
+   * It invokes the 'getDocService' with the request params to retrieve a document.
+   * Once the document is retrieved, it sends a JSON response indicating success.
+   *
+   * @param {Function} getDocService - A service function responsible for fetching document by ID.
+   * @returns {Function} Middleware function for one document retrieval.
+   */
   getById(getDocService) {
     return catchAsync(async (req, res, next) => {
       const doc = await getDocService(req.params.id);
@@ -23,6 +43,16 @@ const factory = {
     });
   },
 
+  /**
+   *
+   * This function is designed to be used as middleware in an Express.js route handler.
+   * It takes a request (req), response (res), and next middleware (next) as parameters.
+   * It invokes the 'updateDocService' with the request params to update a specific document.
+   * Once the document is updated, it sends a JSON response indicating success.
+   *
+   * @param {Function} updateDocService - A service function responsible for updating a document.
+   * @returns {Function} Middleware function for one document updation.
+   */
   updateById(updateDocService) {
     return catchAsync(async (req, res, next) => {
       const updateDoc = await updateDocService(req.params.id, req.body);
@@ -34,6 +64,16 @@ const factory = {
     });
   },
 
+  /**
+   *
+   * This function is designed to be used as middleware in an Express.js route handler.
+   * It takes a request (req), response (res), and next middleware (next) as parameters.
+   * It invokes the 'deleteDocService' with the request params to delete a specific document.
+   * Once the document is deleted, it sends a JSON response indicating success.
+   *
+   * @param {Function} deleteDocService - A service function responsible for deleting a specific document.
+   * @returns {Function} Middleware function for one document deletion.
+   */
   deleteById(deleteDocService) {
     return catchAsync(async (req, res, next) => {
       await deleteDocService(req.params.id);
@@ -45,6 +85,16 @@ const factory = {
     });
   },
 
+  /**
+   *
+   * This function is designed to be used as middleware in an Express.js route handler.
+   * It takes a request (req), response (res), and next middleware (next) as parameters.
+   * It invokes the 'getAllDocsService' function to retrieve all documents.
+   * Once all the documents are retrieved, it sends a JSON response indicating success.
+   *
+   * @param {Function} getAllDocsService - A service function responsible for retrieving all documents in a collection.
+   * @returns {Function} Middleware function for all documents retrieval.
+   */
   getAll(getAllDocsService) {
     return catchAsync(async (req, res, next) => {
       const docs = await getAllDocsService();
--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,14 +1,47 @@
+/**
+ * @file product.service.js
+ * @description This module provides functions to interact with product data in the application. It includes methods for creating, retrieving, updating, and deleting products.
+ */
+
 import Product from "../models/product.model.js";
 import APIError from "../utils/APIError.js";
 
+/**
+ * @typedef {Object} ProductInput
+ * @property {string} title - The title of the product.
+ * ß@property {string} description - The description of the product.
+ * @property {number} unitPrice - The price of the product.
+ * @property {number} unit - The unit of the product.
+ */
+
+/**
+ * @namespace productService
+ */
+
 const productService = {
-  async createProduct(productInput) {
-    const newProduct = new Product(productInput);
-    await newProduct.save();
+  /**
+   * Get a list of all products.
+   * @returns {Promise} A promise that resolves with an array of products or rejects with an error if no products are found.
+   */
+  async getAllProducts() {
+    const products = await Product.find();
+    if (products.length === 0) {
+      throw new APIError({
+        status: 404,
+        message: "There is no document found.",
+        errors,
+      });
+    }
+    return products;
   },
 
+  /**
+   * Get a product by its ID.
+   * @param {string} productId - The ID of the product to retrieve.
+   * @returns {Promise} A promise that resolves with the retrieved product or rejects with an error if not found.
+   */
   async getProduct(productId) {
-    const product = Product.findById(productId);
+    const product = await Product.findById(productId);
     if (!product) {
       throw new APIError({
         status: 404,
@@ -19,8 +52,41 @@ const productService = {
     return product;
   },
 
+  /**
+   * Create a new product with the provided input.
+   * @param {ProductInput} productInput - The product input data.
+   * @returns {Promise} A promise that resolves when the product is successfully created.
+   */
+  async createProduct(productInput) {
+    const newProduct = new Product(productInput);
+    await newProduct.save();
+  },
+
+  /**
+   * Delete a product by its ID.
+   * @param {string} productId - The ID of the product to delete.
+   * @returns {Promise} A promise that resolves with the deleted product or rejects with an error if not found.
+   */
+  async deleteProduct(productId) {
+    const product = await Product.findByIdAndRemove(productId);
+    if (!product) {
+      throw new APIError({
+        status: 404,
+        message: "There is no document found with this ID.",
+        errors,
+      });
+    }
+    return product;
+  },
+
+  /**
+   * Update a product with the provided input by its ID.
+   * @param {string} productId - The ID of the product to update.
+   * @param {ProductInput} productInput - The updated product data.
+   * @returns {Promise} A promise that resolves with the updated product or rejects with an error if not found.
+   */
   async updateProduct(productId, productInput) {
-    const product = Product.findByIdAndUpdate(productId, productInput, {
+    const product = await Product.findByIdAndUpdate(productId, productInput, {
       new: true,
       runValidators: true,
     });
@@ -32,30 +98,6 @@ const productService = {
       });
     }
     return product;
-  },
-
-  async deleteProduct(productId) {
-    const product = Product.findByIdAndRemove(productId);
-    if (!product) {
-      throw new APIError({
-        status: 404,
-        message: "There is no document found with this ID.",
-        errors,
-      });
-    }
-    return product;
-  },
-
-  async getAllProducts() {
-    const products = await Product.find();
-    if (products.length === 0) {
-      throw new APIError({
-        status: 404,
-        message: "There is no document found.",
-        errors,
-      });
-    }
-    return products;
   },
 };
 
