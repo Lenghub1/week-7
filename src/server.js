4787a9d339045b0fbaa7e403aaaf16738408c92e
moonlight_kim <miratorimoonlight@gmail.com>
2023-11-01 18:47:26 +0700

    Add uncaughtException handler in server.js

--- src/server.js
+++ src/server.js
@@ -43,3 +43,10 @@ process.on("unhandledRejection", (err) => {
     process.exit(1);
   });
 });
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
