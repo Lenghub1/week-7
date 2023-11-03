24d3e62576d87159bd7b878b2693473ce84d3d59
SreyNann <soeunsreynan1604@gmail.com>
2023-11-03 14:47:34 +0700

    Create Post schema

---
+++ src/models/post.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import slugify from "slugify";
+
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+      trim: true,
+      min: 3,
+      max: 160,
+    },
+    body: {
+      type: {},
+      require: true,
+      trim: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    media: [
+      {
+        type: Buffer,
+      },
+    ],
+    upvote: {
+      type: Number,
+      default: 0,
+    },
+    downvote: {
+      type: Number,
+      default: 0,
+    },
+    slug: { type: String, unique: true, index: true },
+    excerpt: {
+      type: String,
+      max: 300,
+    },
+  },
+  {
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+);
+postSchema.pre("save", function (next) {
+  this.slug = slugify(this.title, { lower: true });
+  next();
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
