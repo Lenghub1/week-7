66e30ac7c2597d6f790e2ed6aebe263fb0efd513
SereyvatanaUng <sereyvatanau@gmail.com>
2023-11-23 17:20:48 +0700

    delete some comment

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -3,7 +3,6 @@ import usersProductController from "../../controllers/usersProduct.controller.js
 const router = express.Router();
 
 router.route("/").get(usersProductController.getByCategories);
-
 router.route("/hot").get(usersProductController.getNewProducts);
 router.route("/top").get(usersProductController.getTopProducts);
 
