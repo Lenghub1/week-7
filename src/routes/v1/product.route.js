d2d8fff2be79daa33d10cfe7ade6471075d04c16
phirom-02 <phiromkhim02@gmail.com>
2023-12-04 16:40:48 +0700

    refactor: Migrate review operations code from product-route to review-route and merge params.

--- src/routes/v1/product.route.js
+++ src/routes/v1/product.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import usersProductController from "../../controllers/product.controller.js";
+import reviewRoute from "./review.route.js";
 import reviewController from "../../controllers/review.controller.js";
 import isAuth from "../../middlewares/isAuth.js";
 
@@ -9,17 +10,8 @@ router.route("/").get(usersProductController.getByCategories);
 router.route("/all").get(usersProductController.getUserProducts);
 router.route("/hot").get(usersProductController.getHotProducts);
 router.route("/top").get(usersProductController.getTopProducts);
-
 router.route("/:id").get(usersProductController.getProductDetails);
-router
-  .route("/:productId/reviews")
-  .get(reviewController.getReviews)
-  // TODO @later: verify if user purchased the product to be able create to a Review.
-  .post(isAuth, reviewController.createReview);
 
-router
-  .route("/:productId/reviews/:reviewId")
-  .delete(isAuth, reviewController.deleteReview)
-  .patch(isAuth, reviewController.updateReview);
+router.use("/:productId/reviews", reviewRoute);
 
 export default router;
--- src/routes/v1/review.route.js
+++ src/routes/v1/review.route.js
@@ -1,13 +1,18 @@
 import express from "express";
 import reviewController from "../../controllers/review.controller.js";
+import isAuth from "../../middlewares/isAuth.js";
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 router
-  .route("/:productId")
-  .post(reviewController.createReview)
-  .delete(reviewController.deleteReview)
-  .patch(reviewController.updateReview)
-  .get(reviewController.getReviews);
+  .route("/")
+  .get(reviewController.getReviews)
+  // TODO @later: verify if user purchased the product to be able create to a Review.
+  .post(isAuth, reviewController.createReview);
+
+router
+  .route("/:reviewId")
+  .delete(isAuth, reviewController.deleteReview)
+  .patch(isAuth, reviewController.updateReview);
 
 export default router;
